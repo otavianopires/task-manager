18a9e6440d8aa3539755ae78bda2db5694019d20
Otaviano Pires Amancio <otavianopires@gmail.com>
2023-04-12 14:09:29 -0400

    Refactored nextauth scripts

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,18 +1,4 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
-
-export const authOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
-  ],
-}
+import { authOptions } from "@/server/auth";
 
 export default NextAuth(authOptions);
---
+++ server/auth.js
@@ -0,0 +1,36 @@
+
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from "./db"
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: session.email,
+        },
+      })
+
+      if (session) {
+        session.user.id = dbUser.id
+        session.user.name = dbUser.name
+        session.user.email = dbUser.email
+        session.user.image = dbUser.image
+        session.user.role = dbUser.role
+      }
+
+      return session
+    },
+  }
+}
