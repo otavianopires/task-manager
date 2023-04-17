804d1a2959c28c65e40f7f9d56b8533c2dc27d34
Otaviano Pires Amancio <otavianopires@gmail.com>
2023-04-17 13:54:43 -0400

    Add list of 3 latest tasks to dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,12 +1,39 @@
 import { userOptions } from "@/lib/helpers";
+import { prisma } from "@/server/db";
+import Link from "next/link";
 
-export default function DashboardPage() {
+export default function DashboardPage({tasks}) {
+  console.log(tasks);
   return (
     <>
       <h1>Dashboard</h1>
       <p>Welcome to the tasks dashboard</p>
+
+      <h2>Latest tasks</h2>
+      <ul className=" list-inside list-disc">
+        {tasks.map((task, index) => (
+          <li key={task.id}><Link href={`dashboard/tasks/${task.id}`}>{task.title}</Link></li>
+        ))}
+      </ul>
     </>
   )
 }
 
 DashboardPage.auth = userOptions;
+
+export async function getStaticProps() {
+  let tasks = await prisma.task.findMany({
+    orderBy: {
+      startDate: 'desc'
+    },
+    take: 3
+  });
+
+  tasks = JSON.parse(JSON.stringify(tasks));
+
+  return {
+    props: {
+      tasks: tasks
+    },
+  }
+}
