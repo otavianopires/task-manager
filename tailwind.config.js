2459efe786b0a91b9760707bbae55da8d369faa3
Otaviano Pires Amancio <otavianopires@gmail.com>
2023-04-17 13:03:51 -0400

    Define base styles
    - Setup theme for regular and authenticated users.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -12,6 +13,25 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ['var(--font-roboto)'],
+        slab: ['var(--font-roboto-slab)']
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        onPrimary: "var(--color-on-primary)",
+        accentPrimary: "var(--color-accent-primary)",
+        secondary: "var(--color-secondary)",
+        onSecondary: "var(--color-on-secondary)",
+        accentSecondary: "var(--color-accent-secondary)",
+        background: "var(--color-background)",
+        onBackground: "var(--color-on-background)",
+        surface: "var(--color-surface)",
+        onSurface: "var(--color-on-surface)",
+        accentSurface: "var(--color-accent-surface)",
+        darkerSurface: "var(--color-darker-surface)",
+        surfaceBorder: "var(--color-surface-border)",
+      }
     },
   },
   plugins: [],
