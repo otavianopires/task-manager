89cfb1f5eb37287fae89f1a161f15a5fad6ab366
Otaviano Pires Amancio <otavianopires@gmail.com>
2023-04-17 13:23:00 -0400

    Add app description to the HomePage

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import { userOptions } from "@/lib/helpers";
 export default function HomePage() {
   return (
     <>
-      <h1>Welcome to the Task Manager App</h1>
-      <p>Welcome to the tasks dashboard</p>
+      <h1>Welcome to the Task Manager</h1>
+      <p>Task Manager is a web-based application that enables users to stay on top of their daily tasks.</p>
+
+      <p>Developed using Next.js, Tailwind, and Prisma for database management. With a fully-featured login system built with NextAuth.js, users can log in using their Google accounts for easy and secure access.</p>
+
+      <p>The application is designed with a role-based access control system, with default users only able to access the dashboard pages and view their tasks. Admin users, on the other hand, have access to the admin pages, where they can create new tasks and manage the system.</p>
     </>
   )
 }
