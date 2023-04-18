56a838f8ed763cccc449a14cff6b5152e3d378b8
Otaviano Pires Amancio <otavianopires@gmail.com>
2023-04-18 12:57:21 -0400

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -14,6 +14,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session }) {
       const dbUser = await prisma.user.findFirst({
